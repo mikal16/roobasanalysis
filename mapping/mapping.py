d426db0876b42930daf33e9de0c6562bedd8fc4b
mikal16 <66184173+mikal16@users.noreply.github.com>
2024-10-17 14:22:08 -0400

    Update mapping.py

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -7,5 +7,5 @@ for filename in os.listdir('/mnt/data/archive/Smith_B_lungs_2024-08-20/working_c
         r1_name = tokens[0] + "_R1.trimmed.fastq.gz"
         r2_name = tokens[0] + "_R2.trimmed.fastq.gz"
         pf_name = tokens[0]
-        os.system('STAR --genomeDir /mnt/data/genomes/sars_cov_2/ --readFilesCommand zcat --runThreadN 16 --readFilesType Fastx --readFilesIn /mnt/data/archive/Smith_B_lungs_2024-08-20/working_copy/demultiplexing_cutadapt_output/rooba_human_cells/trimmed_fastqfiles/'+r1_name+' /mnt/data/archive/Smith_B_lungs_2024-08-20/working_copy/demultiplexing_cutadapt_output/rooba_human_cells/trimmed_fastqfiles/'+r2_name+' --outFileNamePrefix /mnt/data/archive/Smith_B_lungs_2024-08-20/working_copy/demultiplexing_cutadapt_output/rooba_human_cells/mapped/'+pf_name)
+        os.system('STAR --genomeDir /mnt/data/genomes/hg38_FullRef_OL/ --readFilesCommand zcat --runThreadN 16 --readFilesType Fastx --readFilesIn /mnt/data/archive/Smith_B_lungs_2024-08-20/working_copy/demultiplexing_cutadapt_output/rooba_human_cells/trimmed_fastqfiles/'+r1_name+' /mnt/data/archive/Smith_B_lungs_2024-08-20/working_copy/demultiplexing_cutadapt_output/rooba_human_cells/trimmed_fastqfiles/'+r2_name+' --outFileNamePrefix /mnt/data/archive/Smith_B_lungs_2024-08-20/working_copy/demultiplexing_cutadapt_output/rooba_human_cells/mapped/'+pf_name)
 
